30f1f5748a04db7b0e4b7ed1d9fcc49ee4578026
Manjot Singh <manjot.kalsi@simbaquartz.com>
2026-05-06 12:51:19 +0530

    Add arithmetic operations script to handle sum, product, division, and subtraction

---
+++ scripts/args/index.js
@@ -0,0 +1,38 @@
+// let a = +process.argv[2]
+// let b = +process.argv[3]
+// let c = +process.argv[4]
+
+// let numbers = process.argv
+// let [node,filepath,...numbers] = process.argv
+let numbers = process.argv.slice(3)
+
+let command = process.argv[2]
+console.log(command)
+
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index++) {
+//     const number = numbers[index];
+//     sum += +number
+// }
+if (command === 'sum' || command === 'add') {
+    let sum = numbers.reduce((a, b) => a + +b, 0)
+    console.log(sum)
+}
+if (command === 'product' || command === 'multiply') {
+    let product = numbers.reduce((a, b) => a * +b, 1)
+    console.log(product)
+}
+if (command === 'divide') {
+    if(numbers.length !==2){
+        throw new Error("only 2 numbers can be divided");
+    }
+    let ans = numbers[0] / numbers[1]
+    console.log(ans)
+}
+if (command === 'subtract') {
+    if(numbers.length !==2){
+        throw new Error("only 2 numbers can be subtracted");
+    }
+    let ans = numbers[0] - numbers[1]
+    console.log(ans)
+}
