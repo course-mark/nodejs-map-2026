f85fe05b722d419b6b24d36cd779eeafd0f3eb79
Manjot Singh <manjot.kalsi@simbaquartz.com>
2026-05-08 11:37:25 +0530

    Enhance student entity validation to include unique roll number checks and improve error handling

--- scripts/data-storage/index.js
+++ scripts/data-storage/index.js
@@ -4,7 +4,10 @@ const operationType = process.argv[3]
 
 
 const [, , , , ...properties] = process.argv
-let propertiesInJson = []
+let propertiesInJson = properties.map(prop => ({
+    key: prop.split(":")[0],
+    value: prop.split(":")[1]
+}))
 // check if Data Store exists
 const fs = require('fs')
 
@@ -12,44 +15,45 @@ const fileData = fs.readFileSync('data-store.json', 'utf-8')
 const fileJson = JSON.parse(fileData)
 
 if (entityType === 'student') {
-    propertiesInJson = properties.map(prop => {
+    switch (operationType) {
+        case 'create':
+        case 'add': {
+            propertiesInJson = propertiesInJson.map(({key, value}) => {
+                if (key === "rollNo") {
+                    if (!value) {
+                        throw new Error("rollNo should not be empty student entity");
+                    }
+                    // apply schema validation for unique roll number
+                    if (fileJson?.student?.map(({ rollNo, name }) => rollNo).includes(value)) {
+                        throw new Error(`Roll number ${value} already exists.`);
+                    }
+                }
+                if (key === "name" && !value) {
+                    throw new Error("name should not be empty student entity");
+                }
+                return {
+                    key: key,
+                    value: value
+                }
+            })
 
-        const key = prop.split(":")[0]
-        const value = prop.split(":")[1]
-        if (key === "rollNo") {
-            if (!value) {
-                throw new Error("rollNo should not be empty student entity");
+            const keys = propertiesInJson.map(prop => prop.key)
+            // has name?
+            if (!keys.includes('name')) {
+                throw new Error("name is required for student entity");
             }
-            // apply schema validation for unique roll number
-            if (fileJson?.student?.map(({ rollNo, name }) => rollNo).includes(value)) {
-                throw new Error(`Roll number ${value} already exists.`);
+            if (!keys.includes('rollNo')) {
+                throw new Error("rollNo is required for student entity");
             }
-
-
+            break;
         }
-        if (key === "name" && !value) {
-            throw new Error("name should not be empty student entity");
-        }
-        return {
-            key: key,
-            value: value
-        }
-    })
+        default:
+            break;
 
-const keys = propertiesInJson.map(prop => prop.key)
-    // has name?
-    if (!keys.includes('name')) {
-        throw new Error("name is required for student entity");
-    }
-    if (!keys.includes('rollNo')) {
-        throw new Error("rollNo is required for student entity");
     }
 }
 
 
-// if (!fileJson.student) {
-// if (!fileJson.product) {
-// [] bracket notation is used when we want to access the property of an object using a variable. In this case, entityType is a variable that holds the name of the entity (e.g., "student" or "product"). By using fileJson[entityType], we can dynamically access the property of fileJson that corresponds to the value of entityType.
 if (!fileJson[entityType]) {
     fileJson[entityType] = []
 }
@@ -69,4 +73,7 @@ if (operationType === 'add' || operationType === 'create') {
 
     const jsonstring = JSON.stringify(fileJson, null, 2)
     fs.writeFileSync('data-store.json', jsonstring)
+}
+if (operationType === 'list') {
+    console.log(fileJson[entityType])
 }
